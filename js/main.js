a24887c91d7b2e592312b45d0f36aab1befb4b99
Matt Mokary <mxm6060@rit.edu>
2014-04-04 15:35:42 -0400

    updated main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-
+require(['.requireConfig', 'jquery'], function ($) {
+    $(document).ready(function() {
+        // whatever our main view ends up being
+        // PS guys Alvaro is awesome
+        require(['../app/login']);
+    });
+});
